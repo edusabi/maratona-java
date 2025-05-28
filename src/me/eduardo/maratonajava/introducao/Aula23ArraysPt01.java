fd1ca1b60df64a08e9908c6cffcf958589c78b08
Edu <luis_100404@hotmail.com>
2025-05-28 13:38:06 -0300

    new files

---
+++ src/me/eduardo/maratonajava/introducao/Aula23ArraysPt01.java
@@ -0,0 +1,13 @@
+package me.eduardo.maratonajava.introducao;
+
+public class Aula23ArraysPt01 {
+    public static void main(String[] args) {
+        int []arrayNumbers = new int[10];
+        arrayNumbers[0] = 1;
+        arrayNumbers[1] = 2;
+        arrayNumbers[2] = 3;
+        System.out.println(arrayNumbers[0]);
+        System.out.println(arrayNumbers[1]);
+        System.out.println(arrayNumbers[2]);
+    }
+}
---
+++ src/me/eduardo/maratonajava/introducao/Aula24ArraysPt02.java
@@ -0,0 +1,15 @@
+package me.eduardo.maratonajava.introducao;
+
+public class Aula24ArraysPt02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float, double 0
+        //char '\u0000'
+        //boolean false
+        //String null
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
