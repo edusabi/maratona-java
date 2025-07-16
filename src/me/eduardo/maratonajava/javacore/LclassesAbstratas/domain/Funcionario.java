8a378d2850ebde0c7072c8943aa3f6495a52c649
Edu <luis_100404@hotmail.com>
2025-07-15 22:33:59 -0300

    new files

--- src/me/eduardo/maratonajava/javacore/LclassesAbstratas/domain/Funcionario.java
+++ src/me/eduardo/maratonajava/javacore/LclassesAbstratas/domain/Funcionario.java
@@ -1,12 +1,22 @@
 package me.eduardo.maratonajava.javacore.LclassesAbstratas.domain;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa {
     protected String nome;
     protected double salario;
 
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
+
+    // Classes abstratas PT 02
+    // Métodos abstratos
+    public abstract void calculaBonus();
 
 }
---
+++ src/me/eduardo/maratonajava/javacore/LclassesAbstratas/domain/Pessoa.java
@@ -0,0 +1,5 @@
+package me.eduardo.maratonajava.javacore.LclassesAbstratas.domain;
+
+public abstract class Pessoa {
+    public abstract void imprime();
+}
