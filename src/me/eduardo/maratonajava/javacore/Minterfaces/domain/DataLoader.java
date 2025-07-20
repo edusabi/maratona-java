fc7422b620aba4b88d1e494ca6034e0e56840a79
Edu <luis_100404@hotmail.com>
2025-07-20 16:20:08 -0300

    new files

--- src/me/eduardo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
+++ src/me/eduardo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
@@ -2,4 +2,8 @@ package me.eduardo.maratonajava.javacore.Minterfaces.domain;
 
 public interface DataLoader {
     void load();
+
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro do dataLoader");
+    }
 }
