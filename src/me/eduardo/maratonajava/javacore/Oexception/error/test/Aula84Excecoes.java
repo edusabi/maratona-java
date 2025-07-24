e200b807c0b1b73027d7e494f8a7448c99b6da21
Edu <luis_100404@hotmail.com>
2025-07-24 19:21:04 -0300

    new files

---
+++ src/me/eduardo/maratonajava/javacore/Oexception/error/test/Aula84Excecoes.java
@@ -0,0 +1,13 @@
+package me.eduardo.maratonajava.javacore.Oexception.error.test;
+
+public class Aula84Excecoes {
+    public static void main(String[] args) {
+        // StackOverflowError
+
+//        recursividade();
+    }
+
+    public static void recursividade(){
+//        recursividade();
+    }
+}
---
+++ src/me/eduardo/maratonajava/javacore/Oexception/runTime/test/Aula85ExcecoesPt2.java
@@ -0,0 +1,9 @@
+package me.eduardo.maratonajava.javacore.Oexception.runTime.test;
+
+public class Aula85ExcecoesPt2 {
+    public static void main(String[] args) {
+        // CHECKED E UNCHECKED
+        int[] nums = {1, 2};
+        System.out.println(nums);
+    }
+}
