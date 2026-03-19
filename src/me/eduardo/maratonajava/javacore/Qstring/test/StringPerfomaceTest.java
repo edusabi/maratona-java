45cd95cced792812a5150fe0d8b45a6db81bdd99
Edu <luis_100404@hotmail.com>
2026-03-19 20:22:24 -0300

    add files

---
+++ src/me/eduardo/maratonajava/javacore/Qstring/test/StringPerfomaceTest.java
@@ -0,0 +1,43 @@
+package me.eduardo.maratonajava.javacore.Qstring.test;
+
+public class StringPerfomaceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(3000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasta para String: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasta para StringBuilder: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasta para StringBuffer: " + (fim - inicio) + "ms");
+    }
+
+    private static void concatString(int tamanho){
+        String texto = "";
+
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    private static void concatStringBuffer(int tamanho){
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+}
